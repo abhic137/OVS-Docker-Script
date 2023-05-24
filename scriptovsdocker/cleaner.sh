165283a83fa321f01b642b600dd323c5ed8bad46
abhic137 <46273637+abhic137@users.noreply.github.com>
2023-05-24 16:01:16 +0530

    Update cleaner.sh

--- scriptovsdocker/cleaner.sh
+++ scriptovsdocker/cleaner.sh
@@ -2,6 +2,10 @@ sudo docker kill ovs-vswitchd
 sudo docker rm ovs-vswitchd
 sudo docker kill ovsdb-server
 sudo docker rm ovsdb-server
+sudo docker kill ubuntu1
+sudo docker rm ubuntu1
+sudo docker kill ubuntu2
+sudo docker rm ubuntu2
 
 sudo docker network rm ovs-network1
 sudo docker network rm ovs-network2
