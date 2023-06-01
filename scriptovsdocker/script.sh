fd22c1418da0cc5dc9c25f76062787da5325dceb
abhic137 <46273637+abhic137@users.noreply.github.com>
2023-06-01 10:26:43 +0530

    Update script.sh

--- scriptovsdocker/script.sh
+++ scriptovsdocker/script.sh
@@ -6,7 +6,7 @@ sudo docker network create ovs-comm
 #sudo docker run -itd --net=host --name=ovs-vswitchd --volumes-from=ovsdb-server --privileged openv>
 
 sudo docker run -itd --network ovs-comm --name=ovsdb-server openvswitch/ovs:2.11.2_debian ovsdb-server
-sudo docker run -itd --network ovs-comm --name=ovs-vswitchd --volumes-from=ovsdb-server --privileged openvswitch/ovs: 2.11.2_debian ovs-vswitchd
+sudo docker run -itd --network ovs-comm --name=ovs-vswitchd --volumes-from=ovsdb-server --privileged openvswitch/ovs:2.11.2_debian ovs-vswitchd
 
 
 sudo docker network connect ovs-network1 ovs-vswitchd
